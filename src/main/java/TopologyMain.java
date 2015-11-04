f9855f0f14503b790053db688636d25f7d2f3196
caipanjin <caipanjin@gmail.com>
2015-11-04 18:31:25 +0800

    1.use StormSubmitter 2.add exec-maven-plugin

--- src/main/java/TopologyMain.java
+++ src/main/java/TopologyMain.java
@@ -1,32 +1,33 @@
-import spouts.WordReader;
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import bolts.WordCounter;
 import bolts.WordNormalizer;
+import spouts.WordReader;
 
 
 public class TopologyMain {
-	public static void main(String[] args) throws InterruptedException {
-         
+    public static void main(String[] args) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
+
         //Topology definition
-		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("word-reader",new WordReader());
-		builder.setBolt("word-normalizer", new WordNormalizer())
-			.shuffleGrouping("word-reader");
-		builder.setBolt("word-counter", new WordCounter(),1)
-			.fieldsGrouping("word-normalizer", new Fields("word"));
-		
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("word-reader", new WordReader());
+        builder.setBolt("word-normalizer", new WordNormalizer())
+                .shuffleGrouping("word-reader");
+        builder.setBolt("word-counter", new WordCounter(), 2)
+                .fieldsGrouping("word-normalizer", new Fields("word"));
+
         //Configuration
-		Config conf = new Config();
-		conf.put("wordsFile", args[0]);
-		conf.setDebug(false);
+        Config conf = new Config();
+        conf.put("wordsFile", args[0]);
+        conf.setDebug(false);
         //Topology run
-		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology("Getting-Started-Toplogie", conf, builder.createTopology());
-		Thread.sleep(1000);
-		cluster.shutdown();
-	}
+        conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+        //LocalCluster cluster = new LocalCluster(); //cluster.submitTopology("Count-Word-Topology-With-Refresh-Cache", conf,builder.createTopology());
+        StormSubmitter.submitTopology("Count-Word-Topology", conf,
+                builder.createTopology()); //Thread.sleep(1000); //cluster.shutdown();
+    }
 }
